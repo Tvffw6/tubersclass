b6e39e6ca4b6312090e369fa1f0859ecd13f9cd2
Tvffw6 <bendjiriouy@gmail.com>
2025-12-16 21:15:34 +0100

    Add Tiny Fishing game to the games list

--- public/assets/data/games.js
+++ public/assets/data/games.js
@@ -534,6 +534,12 @@ var games = [
     url: 'https://images-opensocial.googleusercontent.com/gadgets/ifr?url=https://cdn.jsdelivr.net/gh/gertdoro/Toolkit@147a37a734f3e4f155e48dbda25000afdc2970ba/Apps/Helix.xml',
     image: 'https://play-lh.googleusercontent.com/cZHORa1z78dfMsP_nxCkplmHGUx5wfjY0x2jK59GWL7rUL5o6c_KiQ7QL9o4T22qbH8'
   },
+    {
+    id: 'tinyFishing',
+    title: 'Tiny Fishing',
+    url: 'https://55gms.com/misc/tiny-fishing/index.html',
+    image: '/misc/tiny-fishing/thumb.webp'
+  },
   {
     id: 'raccoonretail',
     title: 'Raccoon Retail',
